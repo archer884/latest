7fcd10abf4b629b116035b0e14a964e7a276ba76
J/A <archer884@gmail.com>
2020-04-30 21:07:17 -0500

    Skip hidden files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::fs::{self, DirEntry};
-use std::{env, io};
+use std::path::{Path, PathBuf};
+use std::{env, fs, io};
 use structopt::StructOpt;
 
 #[derive(Clone, Debug, StructOpt)]
@@ -21,20 +21,18 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn list_files(files: impl IntoIterator<Item = DirEntry>) {
-    files
-        .into_iter()
-        .for_each(|x| println!("{}", x.path().display()));
+fn list_files(files: impl IntoIterator<Item = PathBuf>) {
+    files.into_iter().for_each(|x| println!("{}", x.display()));
 }
 
-fn read_files() -> io::Result<impl IntoIterator<Item = DirEntry>> {
+fn read_files() -> io::Result<impl IntoIterator<Item = PathBuf>> {
     let mut entries: Vec<_> = fs::read_dir(env::current_dir()?)?
         .filter_map(|entry| {
-            let entry = entry.ok()?;
+            let entry = entry.ok()?.path();
             let metadata = entry.metadata().ok()?;
             Some((metadata, entry))
         })
-        .filter(|x| x.0.file_type().is_file())
+        .filter(|x| x.0.file_type().is_file() && !is_hidden(&x.1))
         .map(|(meta, entry)| {
             (
                 meta.modified()
@@ -46,4 +44,10 @@ fn read_files() -> io::Result<impl IntoIterator<Item = DirEntry>> {
 
     entries.sort_by(|a, b| b.0.cmp(&a.0));
     Ok(entries.into_iter().map(|(_, x)| x))
+}
+
+fn is_hidden(path: &Path) -> bool {
+    path.file_name()
+        .and_then(|x| x.to_str().map(|x| x.starts_with(".")))
+        .unwrap_or(true)
 }
